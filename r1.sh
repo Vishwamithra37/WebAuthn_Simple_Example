ce3e1a5f9ab1a349b30ef884f08881fb44b84abe
విశ్వామిత్ర <53423141+Vishwamithra37@users.noreply.github.com>
2023-02-15 22:00:55 +0530

    Delete r1.sh

--- r1.sh
+++
@@ -1,2 +0,0 @@
-openssl req -new -newkey rsa:2048 -nodes -keyout jtbi.galam.in.key -out jtbi.galam.in.csr
-openssl x509 -signkey jtbi.galam.in.key -in jtbi.galam.in.csr -req -days 3650 -out jtbi.galam.in.crt
